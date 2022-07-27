e514b6f928938cc5683e5d4cd7d08e8b77dc1ae3
Sefa-Enes <73657105+Sefa-Enes@users.noreply.github.com>
2022-07-27 23:42:18 +0300

    on hazirlik

--- body.py
+++ body.py
@@ -1,4 +1,9 @@
 def sample():
+<<<<<<< HEAD
+    print('S.A.')
+    
+=======
     print('A.S.')
+>>>>>>> 67e57ca5ae8c0d24e9ebab4e95e20af947b5c6e0
     #! Test for Furkan
     #? TEst for if it effects to my desktop
