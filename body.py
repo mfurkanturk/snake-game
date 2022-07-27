bc65e867027562fd491d1e7bcac6963c03ee6260
Sefa-Enes <73657105+Sefa-Enes@users.noreply.github.com>
2022-07-27 23:47:48 +0300

    on hazirlik

--- body.py
+++ body.py
@@ -1,9 +1,7 @@
-def sample():
-<<<<<<< HEAD
-    print('S.A.')
-    
-=======
-    print('A.S.')
->>>>>>> 67e57ca5ae8c0d24e9ebab4e95e20af947b5c6e0
-    #! Test for Furkan
-    #? TEst for if it effects to my desktop
+
+def drawBody():               #TODO: Her turda tekrar cizilecek body, skor miktari kadar 
+    pass
+
+
+def bodyCol():                #TODO: Bir sonraki adimda yilanin basi body olan bir kareye geliyorsa end fonksiyonunu cagir   
+    pass
