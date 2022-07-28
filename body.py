a8ec2bd3054e8d27531e38de3a6bd5995d5babed
Sefa-Enes <73657105+Sefa-Enes@users.noreply.github.com>
2022-07-28 22:04:11 +0300

    Furkan icin update

--- body.py
+++ body.py
@@ -1,7 +1,13 @@
 
-def drawBody():               #TODO: Her turda tekrar cizilecek body, skor miktari kadar 
-    pass
+def drawBody(score, ):               #TODO: Her turda tekrar cizilecek body, skor miktari kadar 
+    location = []
+    location[0] = [1, 2]
+    
+    return location
 
 
-def bodyCol():                #TODO: Bir sonraki adimda yilanin basi body olan bir kareye geliyorsa end fonksiyonunu cagir   
-    pass
+def bodyCol(location):                #TODO: Bir sonraki adimda yilanin basi body olan bir kareye geliyorsa end fonksiyonunu cagir   
+    for i in range(len(location)):
+        if location[0] == location[i]:
+            endgame()                 #! Endgame function will be here
+            
