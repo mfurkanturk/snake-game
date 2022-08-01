793d677c26319d54a22a69887cbd8d89e6dff52b
iamgorkemavci <gorkemvci@gmail.com>
2022-08-01 17:24:10 +0300

    .

--- body.py
+++ body.py
@@ -52,39 +52,3 @@ def drawBody(score):               #TODO: Her turda tekrar cizilecek body, skor 
         screen.onkey(right, "Right") 
         screen.onkey(left, "Left") 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def drawBody(score):               #TODO: Her turda tekrar cizilecek body, skor miktari kadar 
-#     snake = []
-#     for i in range(score):         #* Skor miktarina gore uzunluk
-#         snake.append('')
-#         snake[i] = Turtle()
-#         snake[i].shape('circle')
-#         snake[i].penup()
-#     while True:
-#         xa = snake[0].xcor()
-#         ya = snake[0].ycor()
-#         snake[0].forward(20)
-    
-#         for i in range(1,10):
-#             xb = snake[i].xcor()
-#             yb = snake[i].ycor()
-#             snake[i].goto(xa,ya)
-#             xa = xb
-#             ya = yb    
