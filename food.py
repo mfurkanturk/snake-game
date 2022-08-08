1dd2d58ece8b723c0625eac4f68a9b8d5b14202d
iamgorkemavci <gorkemvci@gmail.com>
2022-08-08 10:12:31 +0300

    Enes'in ve benim kodlarım yeniden düzenlendi ve OPPye uygun hale geli.

--- food.py
+++ food.py
@@ -1,15 +1,19 @@
-from turtle import Screen, Turtle
-import random 
-def food():
-    #screen =Screen()
-    #screen.screensize(canvwidth=600, canvheight=600, bg="green")
-    turtle = Turtle()
-    turtle.shape("circle")
-    turtle.penup()
-    turtle.shapesize(stretch_len=0.8,stretch_wid=0.8)
-    turtle.color("black")
-    apsis = random.randint(-300,300)
-    ordinat = random.randint(-300,300)
-    turtle.goto(apsis,ordinat)
-    return turtle
-    
+from turtle import Turtle
+from random import randint
+class Food(Turtle): #class yapısı kodların birbirleriyle iletişimini kolaylaştıracaktır
+    def __init__(self) : #nesne yaratılması ile birlikte gelen oto durumlar
+        super().__init__() #turtle nesnesinden inherit edilerek turtle fonskiyonlarının kullanılması sağlanmıştır
+        self.shape("circle")
+        self.shapesize(1)
+        self.penup()   
+        self.color("Black")  
+    def fooding(self):
+        x = randint(-270,270)
+        y = randint(-270,270)
+        self.goto(x,y)
+    def clearfooding(self):
+        self.clear()
+        x = randint(-270,270)
+        y = randint(-270,270)
+        self.goto(x,y)
+       
--- main.py
+++ main.py
@@ -1,15 +1,66 @@
-from turtle import Screen
-import body as mv
+from turtle import Screen, Turtle
+from snake_body import SnakeBody
+from food import Food
+import time
+my_screen = Screen()
+my_screen.tracer(0) #tracer görüntü akşını keser 
+my_screen.setup(width=600,height=600) 
+my_screen.title("Snake Game")
+my_screen.bgcolor("lightblue")
+snake = SnakeBody()
+snake.hideturtle() 
+snake.create_snake() # 3 segmentli bir yılan yaratılır
+my_screen.listen() #tuşları aktif hale getirir
+my_screen.tracer(0) #tracer görüntü akşını keser
+def up_fun():
+    headings =snake.snake_segment[0].heading()
+    if headings != 270.0 : #yılanın kendi içinden geçmesi engellenmiştir.
+       snake.snake_segment[0].setheading(90)
+def down_fun():
+    headings =snake.snake_segment[0].heading()
+    if headings != 90.0 :#yılanın kendi içinden geçmesi engellenmiştir.
+        snake.snake_segment[0].setheading(270)
+def east_fun():
+    headings =snake.snake_segment[0].heading()
+    if headings != 180.0 :#yılanın kendi içinden geçmesi engellenmiştir.
+        snake.snake_segment[0].setheading(0)
+def west_fun():
+    headings =snake.snake_segment[0].heading()
+    if headings != 0.0 and 360.0 :#yılanın kendi içinden geçmesi engellenmiştir.
+        snake.snake_segment[0].setheading(180)
+my_screen.update() #update olay örgüsü gerçekleştikten görüntü akışını günceller
+my_screen.onkey(up_fun,"w")
+my_screen.onkey(down_fun,"s")
+my_screen.onkey(east_fun,"d")
+my_screen.onkey(west_fun,"a")
 
-
-#screen = Screen() 
-#screen.screensize(canvwidth=600, canvheight=600, bg="green")
-#screen.title("Snake game")
-
-
-# screen.listen()
-
-mv.drawBody(5) #Örnek skor girdisi 5
-
+food = Food() # yılanın yemi classtan çağrılır
+food.fooding() # random bir alana yılan yemi gelir
+my_screen.update()#update olay örgüsü gerçekleştikten görüntü akışını günceller
+score = Turtle()  #score tablosu yaratılır
+score.hideturtle()
+score.penup()
+score.goto(0,250)
+control = True #While Döngüsünü kontrol eder
+score_num = 0
+while control ==  True :
+    my_screen.tracer(0) #tracer görüntü akşını keser (döngü tamamlana kadar)
+    snake.snake_move()
+    if food.distance(snake.snake_segment[0]) < 18:
+        snake.snake_food() #yılanın boyu uzatılır 
+        food.clearfooding() #yılanın yemi silinir ve random konuma yeni bie yem bırakılır
+        score_num = score_num+1 #skor tablosuna ekleme yapılır
+        score.clear()
+        score.write(f"Score: {score_num}", align="center", font=('Arial',20,'bold') )
+    #Buraya if yapısı ile birlikte distance kullanmak yerine x ve y kordinatlra olan uzaklıklık hesabı yapılması gerekir
+    #bkz. xcor ve ycor fonksiyonları 
+    #yılanın ekranın duvarlarına çarptığı zaman oyunun bitmesi sağlanacak.
+    #Buraya for döngüsü ve if yapısı ile distance fonksiyonu kullanılarak yılanın kendi segmentlerine çarptığı zaman oyunun bitmesi sağlanacak.
+    # distance kullanırken for döngüsünde yılanın kendi segmentini çıkartmayı unutmakamk gerekir. segemnt[1]den başlaması lazım yoksa 
+    #sürekli yılanın başı kendıne çarpacaktır.
+    my_screen.update() #while döngüsünün sonunda ekran güncellenir
+    time.sleep(0.1) 
+#Görkem AVCI 
+my_screen.exitonclick()
 
 
