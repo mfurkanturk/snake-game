a6ece8cd7f73b079c53f1523ece2aea6fb1ab9b1
M.Furkan Türk <92334781+mfurkanturk@users.noreply.github.com>
2022-08-09 17:16:30 +0300

    finished

--- food.py
+++ food.py
@@ -1,19 +1,24 @@
 from turtle import Turtle
 from random import randint
-class Food(Turtle): #class yapısı kodların birbirleriyle iletişimini kolaylaştıracaktır
-    def __init__(self) : #nesne yaratılması ile birlikte gelen oto durumlar
-        super().__init__() #turtle nesnesinden inherit edilerek turtle fonskiyonlarının kullanılması sağlanmıştır
+
+
+class Food(Turtle):  # class yapısı kodların birbirleriyle iletişimini kolaylaştıracaktır
+
+    def __init__(self) :  # nesne yaratılması ile birlikte gelen oto durumlar
+        super().__init__()  # turtle nesnesinden inherit edilerek turtle fonskiyonlarının kullanılması sağlanmıştır
         self.shape("circle")
         self.shapesize(1)
         self.penup()   
-        self.color("Black")  
+        self.color("Black")
+
     def fooding(self):
         x = randint(-270,270)
-        y = randint(-270,270)
+        y = randint(-270,240)
         self.goto(x,y)
+
     def clearfooding(self):
         self.clear()
         x = randint(-270,270)
-        y = randint(-270,270)
+        y = randint(-270,240)
         self.goto(x,y)
        
--- snake_body.py
+++ snake_body.py
@@ -1,27 +1,28 @@
 from turtle import Turtle
-class SnakeBody(Turtle): #class yapısı kodların birbirleriyle iletişimini kolaylaştıracaktır
-    def __init__(self) :#nesne yaratılması ile birlikte gelen oto durumlar
-        super().__init__()#turtle nesnesinden inherit edilerek turtle fonskiyonlarının kullanılması sağlanmıştır
+
+
+class SnakeBody(Turtle):  # class yapısı kodların birbirleriyle iletişimini kolaylaştıracaktır
+    def __init__(self) :  # nesne yaratılması ile birlikte gelen oto durumlar
+        super().__init__()  # turtle nesnesinden inherit edilerek turtle fonskiyonlarının kullanılması sağlanmıştır
         self.penup()
         self.shape("square")
         self.snake_segment = []
         
     def create_snake(self):
         x_cor = 0
-        for _ in range(3):
+        for n in range(3):
             my_snake = SnakeBody()
             my_snake.fillcolor("Black")
             my_snake.goto(x_cor,0)
             self.snake_segment.append(my_snake)
-            
-    
+
     def snake_move(self):
-            head_position = self.snake_segment[0].position()
-            self.snake_segment[0].forward(20)
-            for _ in range(len(self.snake_segment)-1):
-                n_head_pos = self.snake_segment[_ + 1].position()
-                self.snake_segment[_ + 1].goto(head_position)
-                head_position = n_head_pos
+        head_position = self.snake_segment[0].position()
+        self.snake_segment[0].forward(20)
+        for n in range(len(self.snake_segment)-1):
+            n_head_pos = self.snake_segment[n + 1].position()
+            self.snake_segment[n + 1].goto(head_position)
+            head_position = n_head_pos
             
     def snake_food(self):
         my_snake = SnakeBody()
